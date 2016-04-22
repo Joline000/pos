56290314e068d462da8d0ebda3e6b94571ef38b5
YourName <YouEmailAddress>
2016-04-22 16:13:56 +0800

    v0.1 提交

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,4 +1,67 @@
-//TODO: Please write code in this file.
 function printReceipt(inputs) {
+    var items = buildItems(inputs);
+    var cartItems = buildCartItems(items);
+    var receiptItem = buildReceiptItems(cartItems);
+    var receipt = getReceipt(receiptItem);
+    console.log(receipt);
+}
 
+function buildItems(Item) {
+    var cartItems = [];
+    
+    Item.forEach(function(cart) {
+        var elementExist = isBarcodeExist(cart, cartItems);
+        if(elementExist) {
+            elementExist.count++;
+        }
+        else {
+            cartItems.push({input: cart, count: 1});
+        }
+    });
+    
+    return cartItems;
+}
+
+function isBarcodeExist(cart, barcodes) {
+    var elementExist;
+    barcodes.forEach(function(items) {
+        if(cart.barcode === items.input.barcode) {
+            elementExist = items;
+        }
+    });
+    
+    return elementExist;
+}
+
+function buildCartItems(cartItems) {
+    var cart = [];
+    cartItems.forEach(function(Item) {
+        cart.push({cartItems: Item, subtotal: Item.input.price * Item.count});
+    });
+    
+    return cart;
+}
+
+function buildReceiptItems(cartItems) {
+    var receiptItem = [cartItems];
+    var total = 0;
+    cartItems.forEach(function(cartItems) {
+        total += cartItems.subtotal;
+    });
+    
+    receiptItem.push(total);     
+    
+    return receiptItem;
+}
+
+function getReceipt(receiptItem) {
+    var receipt = '***<没钱赚商店>收据***\n';
+    receiptItem[0].forEach(function(item) {
+        receipt += '名称：'+ item.cartItems.input.name +'，数量：'+ item.cartItems.count + item.cartItems.input.unit +'，单价：'
+        + item.cartItems.input.price.toFixed(2) + '(元)，小计：' + item.subtotal.toFixed(2) +'(元)\n';
+    });
+    receipt += '----------------------\n';
+    receipt += '总计：'+ receiptItem[1].toFixed(2) + '(元)\n**********************';
+    
+    return receipt;
 }
--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -9,32 +9,32 @@ function buildCartItems(Items) {
 
     var cartItems = [];   
     Items.forEach(function(Item) {
-        cartItems.push({info: Item, subtotal: Item.price * Item.count});
+        cartItems.push({inputs: Item, subtotal: Item.price * Item.count});
     });
     
    return cartItems;
 }
 
 function buildReceiptItems(cartItems) {
-    var receiptItems = [cartItems];
+    var receiptItem = [cartItems];
     var total = 0;
     cartItems.forEach(function(cartItems) {
         total += cartItems.subtotal;
     });
     
-    receiptItems.push(total);     
+    receiptItem.push(total);     
     
-    return receiptItems;
+    return receiptItem;
 }
 
-function getReceipt(receiptItems) {
+function getReceipt(receiptItem) {
     var receipt = '***<没钱赚商店>收据***\n';
-    receiptItems[0].forEach(function(cartItems) {
-        receipt += '名称：'+ cartItems.info.name +'，数量：'+ cartItems.info.count + cartItems.info.unit +'，单价：'
-        + cartItems.info.price.toFixed(2) + '(元)，小计：' + cartItems.subtotal.toFixed(2) +'(元)\n';
+    receiptItem[0].forEach(function(cartItems) {
+        receipt += '名称：'+ cartItems.inputs.name +'，数量：'+ cartItems.inputs.count + cartItems.inputs.unit +'，单价：'
+        + cartItems.inputs.price.toFixed(2) + '(元)，小计：' + cartItems.subtotal.toFixed(2) +'(元)\n';
     });
-    receipt += '----------------------\n';
-    receipt += '总计：'+ receiptItems[1].toFixed(2) + '(元)\n**********************';
+    receipt += '----------------------\n'
+    receipt += '总计：'+ receiptItem[1].toFixed(2) + '(元)\n**********************';
     
     return receipt;
 }
